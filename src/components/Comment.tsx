3914a1e3915b7d0333f91f259f4c4756123ea37a
Gustavo Albunio <gustavo.albunio@gmail.com>
2022-12-21 17:57:01 -0300

    ignite-feed

---
+++ src/components/Comment.tsx
@@ -0,0 +1,59 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleLikeComment() {
+    setLikeCount((state) => state + 1);
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src='https://github.com/gustavoalbunio.png'
+        alt=''
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gustavo Albunio</strong>
+              <time
+                title='11 de Dezembro às 8:00h'
+                dateTime='2022-12-20 08:00:00'
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button
+              onClick={() => onDeleteComment(content)}
+              title='Deletar comentário'
+            >
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
